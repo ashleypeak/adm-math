ea5b2bbc6ef537af89f776a62b3080328ee52f4b
Wyatt Peak <wyattpeak@gmail.com>
2019-01-05 13:56:33 +1100

    implemented copy function
    copies selection to clipboard in OpenMath

--- js/adm-math-input.js
+++ js/adm-math-input.js
@@ -121,12 +121,14 @@
 		inputTemplate += " ng-keypress=\"control.keypress($event)\"";
 		inputTemplate += " ng-keydown=\"control.keydown($event)\"";
 		inputTemplate += " ng-focus=\"control.focus()\"";
-		inputTemplate += " ng-blur=\"control.blur()\">";
+		inputTemplate += " ng-blur=\"control.blur()\"";
+		inputTemplate += " ng-copy=\"control.copy()\">";
 		inputTemplate += "<adm-math-expression";
 		inputTemplate += " cursor=\"cursor\"";
 		inputTemplate += " expression=\"literalTree\"";
 		inputTemplate += " control=\"control\"></adm-math-expression>";
 		inputTemplate += "<input type=\"hidden\" name=\"{{name}}\" value=\"{{model}}\" />";
+		inputTemplate += "<input type=\"hidden\" class=\"clipboard\" />";
 		inputTemplate += "</div>";
 		
 		$templateCache.put("adm-math-expression.htm", expressionTemplate);
@@ -174,9 +176,7 @@
 				modelOm: "=?admModelOm",
 				modelLatex: "=?admModelLatex",
 				functions: "@?admFunctions",
-				hook: "=?admHook",
-				onFocus: "&?admFocus",
-				onBlur: "&?admBlur"
+				hook: "=?admHook"
 			},
 			templateUrl: "adm-math-input.htm",
 			link: function(scope, element, attrs) {
@@ -295,6 +295,7 @@
 				 *							`blur`						returns none
 				 *							`keypress`				returns none
 				 *							`keydown`					returns BOOLEAN | none
+				 *							`copy`						returns none
 				 *							`nodeClick`				returns none
 				 *							`nodeMousedown`		returns none
 				 *							`nodeMouseup`			returns none
@@ -363,6 +364,9 @@
 					 * return:			none
 					 ******************************************************************/
 					keypress: function(e) {
+						if(e.ctrlKey)	//don't capture control combinations
+							return;
+						
 						var character = String.fromCharCode(e.which);
 						if(/[a-zA-Z0-9.+\-*()\^|,='<>~]/.test(character)) {
 							scope.cursor.insert(character);
@@ -408,6 +412,42 @@
 					},
 
 					/*******************************************************************
+					 * function:		copy()
+					 *
+					 * description:	run on ngCopy of math input field
+					 *							copy selected content in a format which will allow
+					 *							pasting in control.paste().
+					 *							copying in a useful format for other pasting
+					 *							elsewhere is a secondary priority
+					 *
+					 * arguments:		none
+					 *
+					 * return:			none
+					 ******************************************************************/
+					copy: function() {
+						if(!scope.cursor.hasSelection)
+							return;
+						
+						var copyLiteral = admLiteralNode.buildBlankExpression(null);
+						
+						for(var i = scope.cursor.selectionRange.start; i <= scope.cursor.selectionRange.end; i++)
+							copyLiteral.insert(i-scope.cursor.selectionRange.start, scope.cursor.expression.getNode(i-1));
+						
+						var semantic = admLiteralParser.getAdmSemantic(copyLiteral, registeredFunctions);
+						var om = semantic.getOpenMath();
+						
+						var el = document.createElement('textarea');
+						el.value = om;
+						el.setAttribute('readonly', '');
+						el.style = {position: 'absolute', left: '-9999px'};
+						
+						document.body.appendChild(el);
+						el.select();
+						document.execCommand('copy');
+						document.body.removeChild(el);
+					},
+
+					/*******************************************************************
 					 * function:		nodeClick()
 					 *
 					 * description:	run when an individual node element is clicked
@@ -437,6 +477,7 @@
 					 ******************************************************************/
 					nodeMousedown: function(event, node, nodeIndex) {
 						event.preventDefault();
+						element[0].focus(); //need to focus explicitly since bubbling prevented, otherwise input can lack focus even after selecting
 						
 						this.selecting = true;
 						
